839c6bd54c12b53a74e44d36d829acf734efdc0b
divyang-mobio <107481877+divyang-mobio@users.noreply.github.com>
2023-01-16 16:10:02 +0530

    Update data_model.dart

--- lib/model/data_model.dart
+++ lib/model/data_model.dart
@@ -1,9 +1,9 @@
 class DataModel {
   String title, image, description, type;
-  int pric;
+  int price;
 
   DataModel(
-      {required this.price,
+      {required this.pric,
       required this.image,
       required this.description,
       required this.title,
